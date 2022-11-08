f90f76b2e3e5ad0e744fcc6f0adaa0dc1fcb00a5
Amoss Dvir <amoss.dvir@verint.com>
2022-11-08 19:23:36 +0200

    Menu and rearrangements done

---
+++ src/components/menu/MenuItems.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Tab } from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+
+
+const MenuItems = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default MenuItems;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import "./index.css";
-import Store from "./model/Store"
+import Store from "./model/Store";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<Provider store={Store}>
-  <BrowserRouter>
-    <App />
+  <Provider store={Store}>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </Provider>
 );
