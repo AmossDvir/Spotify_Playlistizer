13c1e3d455e6046bb775333124e22aa9907f48d3
AmossDvir <63615747+AmossDvir@users.noreply.github.com>
2022-12-04 13:20:13 +0200

    Delete src directory

--- src/model/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
-import globalStateReducer from "./globalStateSlice"
-
-const Store = configureStore({
-  reducer: { user: userReducer, globalState: globalStateReducer },
-});
-
-export default Store;
