ff315b3974d3283d57c7d00f8f4b4202ad17a081
Amoss Dvir <amoss.dvir@verint.com>
2022-12-07 12:57:09 +0200

    Home Page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,27 +3,41 @@ import MovingComponent from "react-moving-text";
 import { Box } from "@mui/material";
 import AnimatedText from "../generalComponents/AnimatedText";
 
+const title = "Welcome.";
+const subTitle = "This tool will help you create playlists by your favorite genres";
 const Home = () => {
   var time = 0;
+  setTimeout(() => sessionStorage.setItem("firstTime", true), 2000);
   return (
-    // <Box alignItems="center" display="flex" mt={10} flexDirection="column" fontSize={30} fontFamily="Jost">
-
-    <AnimatedText
-      textLines={[
-        { value: "Welcome.", style: { fontSize: 50 }, delay: 1 },
-        {
-          value:
-            "This tool will help you create your playlists by your favorite genres",
-          style: { fontSize: 35 },
-          delay: 1.5,
-        },
-      ]}
-      time={time}
-    >
-      Welcome.
-    </AnimatedText>
-
-    // </Box>
+    <Box justifyContent="center" display="flex" fontSize={30} fontFamily="Jost">
+      {!sessionStorage.getItem("firstTime") ? (
+        <AnimatedText
+          textLines={[
+            { value: title, style: { fontSize: 50 }, delay: 1 },
+            {
+              value:subTitle,
+              style: { fontSize: 35 },
+              delay: 1.5,
+            },
+          ]}
+          time={time}
+        >
+          Welcome.
+        </AnimatedText>
+      ) : (
+        <div>
+          <Box
+            alignItems="center"
+            display="flex"
+            mt={10}
+            flexDirection="column"
+            fontSize={30}
+            fontFamily="Jost"
+            style={{ cursor: "default" }}
+          ><div style={{fontSize:50}}>{title}</div><div>{subTitle}</div></Box>
+        </div>
+      )}
+    </Box>
   );
 };
 
